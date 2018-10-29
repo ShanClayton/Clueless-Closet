89584b54be35043a918921495681b084c97f09ea
Stevie Burnette <Diamond@Mitchells-MacBook-Pro.local>
2018-10-29 13:08:04 -0400

    merging partials

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,7 @@ app.controller('MyController', ['$http', function($http){
     const controller = this;
 
     this.newOutfit = function(){
+      console.log(this.type);
       $http({
         method: 'POST',
         url: '/outfits/new',
@@ -55,14 +56,16 @@ app.controller('MyController', ['$http', function($http){
         }
 
     this.editOutfit = function(outfit){
+      console.log(this.editedImage);
       $http({
         method: 'PUT',
         url: '/outfits/' + outfit._id,
         data: {
-          category: this.editedcategory,
-          type: this.editedtype,
-          image: this.editedImage,
-          tag: this.editedtag
+          category: "hey",
+          // type: this.editedtype,
+          image: this.image,
+
+          // tag: this.editedtag
         }
       }).then(function(response){
         // this.editedcategory = null,
